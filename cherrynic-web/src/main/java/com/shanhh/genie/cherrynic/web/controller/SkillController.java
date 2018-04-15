0d70c2a24c86b00a405fcb7cd3ae5929cb9fdfe1
dan <i@shanhh.com>
2018-04-15 11:39:03 +0800

    feature add empty controller

--- cherrynic-web/src/main/java/com/shanhh/genie/cherrynic/web/controller/SkillController.java
+++ cherrynic-web/src/main/java/com/shanhh/genie/cherrynic/web/controller/SkillController.java
@@ -1,6 +1,8 @@
 package com.shanhh.genie.cherrynic.web.controller;
 
+import com.alibaba.da.coin.ide.spi.standard.ResultModel;
 import com.alibaba.da.coin.ide.spi.standard.TaskQuery;
+import com.alibaba.da.coin.ide.spi.standard.TaskResult;
 import com.alibaba.da.coin.ide.spi.trans.MetaFormat;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +14,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Slf4j
 public class SkillController {
 
-    public void genieCall(@RequestBody String taskQuery) {
+    public ResultModel<TaskResult> genieCall(@RequestBody String taskQuery) {
         log.info("taskQuery: {}", taskQuery);
         TaskQuery query = MetaFormat.parseToQuery(taskQuery);
 
+        ResultModel<TaskResult> resultModel = new ResultModel<TaskResult>();
+
+        TaskResult result = new TaskResult();
+        resultModel.setReturnCode("0");
+        resultModel.setReturnValue(result);
+        return resultModel;
     }
+    
 }
